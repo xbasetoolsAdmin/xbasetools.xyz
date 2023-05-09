9920f3a095395988b0a17fed3cc85722f627bfa3
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-09 22:56:27 +0100

    Update divPage8.php

--- buyer/divPage8.php
+++ buyer/divPage8.php
@@ -22,8 +22,54 @@ if(!isset($_SESSION['sname']) and !isset($_SESSION['spass'])){
 }
   include "../includes/config.php";
 ?>
-<?php
 
+
+<?php
+	///////////////// premium
+ if (
+	 $row['type'] == "account") {
+	 $itemid = $row['s_id'];
+$qe = mysqli_query($dbcon, "SELECT * FROM accounts WHERE id='$itemid'") or die(mysql_error());
+while ($rowe = mysqli_fetch_assoc($qe)) {
+
+$country = $rowe['country'];
+$site = $rowe['sitename'];
+$description = $rowe['infos'];
+$information = $rowe['url'];
+$code = array_search("$country", $countrycodes);
+ $countrycode = strtolower($code);
+?>
+                  <h4>PREMIUM</h4>
+
+
+<table class="table">
+<tr>
+  <td>Country</td>
+  <td><b><span class="flag-icon flag-icon-<?php echo htmlspecialchars($countrycode); ?>"></span> <?php echo htmlspecialchars($country); ?></b></td>
+</tr>
+
+  <tr>
+  <td>Available Information</td>
+  <td><b><?php echo htmlspecialchars($description); ?></b></td>
+</tr>
+
+  <tr>
+  <td>Website</td>
+  <td><b><a><?php echo htmlspecialchars($site); ?></a></b></td>
+</tr>
+
+  <tr>
+  <td>Account Info</td>
+  <td><b><textarea rows='10' cols='30' ><?php echo htmlspecialchars($information); ?></textarea></b></td>
+</tr>
+
+  		
+</table>
+<?php
+}
+	 }
+	 //////////////End if premium
+	 ?>
   $date = date("Y-m-d H:i:s");
   $uid = mysqli_real_escape_string($dbcon, $_GET['id']);
   $tbl = mysqli_real_escape_string($dbcon, $_GET['t']);
