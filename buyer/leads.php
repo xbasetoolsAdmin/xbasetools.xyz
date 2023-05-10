64a0d92ca4008c7dc97d78f630dfcaa73e4bdefe
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-10 12:08:55 +0100

    Update leads.php

--- buyer/leads.php
+++ buyer/leads.php
@@ -10,27 +10,28 @@ if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
 }
 $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 ?>
-<!doctype html>
+<!DOCTYPE html>
 <html>
+
 <head>
-<link rel="stylesheet" type="text/css" href="files/bootstrap/3/css/bootstrap.css?1" />
-<link rel="stylesheet" type="text/css" href="files/css/flags.css" />
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.min.css">
-<script type="text/javascript" src="files/js/jquery.js?1"></script>
-<script type="text/javascript" src="files/bootstrap/3/js/bootstrap.js?1"></script>
-<script type="text/javascript" src="files/js/sorttable.js"></script>
-<script type="text/javascript" src="files/js/table-head.js?3334"></script>
-<script type="text/javascript" src="files/js/bootbox.min.js"></script>
-<script type="text/javascript" src="files/js/clipboard.min.js"></script>
+         <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+         <meta name="alfacoins-site-verification" content="welcome">
+         <meta name="revisit-after" content="2 days">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+         <script src="/cdn-cgi/apps/head/AGN3NmUziwZfMV-TNTe1kdyeR2Y.js"></script>
+        <link rel="shortcut icon" href="../../favicon.ico" />    
+	<title>Ticket</title>
+        <link rel="stylesheet" type="text/css" href="filea/css/bootstrap.min.css">
+	<script  type"text/javascript" src="files/js/bootbox.min.js"></script>   
+	<script type="text/javascript" src="files/js/clipboard.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="files/css/flags.css" />
+	<script type="text/javascript" src="files/js/bootstrap.min.js"></script>
 
-<link rel="shortcut icon" href="files/img/favicon.ico" />
-<meta http-equiv="X-UA-Compatible" content="IE=10; IE=9; IE=8; IE=7; IE=EDGE" /> 
- <meta name="referrer" content="no-referrer" />
+       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+       <script type="text/javascript" src="files/js/jquery.js"></script>
+     <script type="text/javascript" src="files/js/jquery.dataTables.min.js"></script>
 
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta charset="utf-8">
-<title>JeruxShop</title>
-</head>
 <style>
 #table {
   .sortable
