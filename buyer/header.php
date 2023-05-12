5cbd076f4b4f923c8cc001f5da0a774f3351c5f7
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-12 09:57:36 -0700

    Update header.php

--- buyer/header.php
+++ buyer/header.php
@@ -20,8 +20,26 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
     <link rel="shortcut icon" href="favicon.ico.png">
  
     <title>Ticket</title>
-    <link rel="stylesheet" href="layout/css/all.xbasetools.css">
-    <link rel="stylesheet" href="layout/css/bs.xbasetools.min.css">
+    <link rel="stylesheet" href="layout/css/all.xbasetools.css">\
+    
+<html>
+	<head>
+		<title>How to Dynamically Display Hide DataTables Column with PHP Ajax</title>
+		<!-- JS, Popper.js, and jQuery -->
+		<script  src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+		<!-- CSS only -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+		
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+		<script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+		<script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>  
+		<link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css" />
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
+  		<script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>
+   
+        <link rel="stylesheet" href="layout/css/bs.xbasetools.min.css">
     <link rel="stylesheet" href="layout/css/bs.main.css">
     <link rel="stylesheet" href="layout/css/bs.xbasetools.css">
     <link rel="stylesheet" href="layout/fonts/iconic/css/material-design-iconic-font.min.css">
