efb8e1a754a9d6339a593c42813d1d5d7ac3e681
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-12 08:27:09 -0700

    Update header.php

--- buyer/header.php
+++ buyer/header.php
@@ -1,4 +1,17 @@
-DOCTYPE html><html class="theme-light"><head>
+
+<?php
+ob_start();
+session_start();
+date_default_timezone_set('UTC');
+include "../includes/config.php";
+if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
+    header("location: login");
+    exit();
+}
+$usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
+?>
+
+<!DOCTYPE html><html class="theme-dark"><head>
 <meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="revisit-after" content="2 days">
 <meta http-equiv="X-UA-Compatible" content="IE=edge"><link rel="shortcut icon" href="favicon.ico.png">
