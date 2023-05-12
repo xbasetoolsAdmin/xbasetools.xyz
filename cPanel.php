09316756a558644a8fc7bfb9addd8c14158dd962
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-12 10:46:31 -0700

    Update cPanel.php

--- cPanel.php
+++ cPanel.php
@@ -35,15 +35,7 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 
 
 
-.ajax-loader {
-  position: absolute;
-  left: 0;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  margin: auto; /* presto! */
 
-}
 
    
     
