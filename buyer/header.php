5a524f77c78f1ae5af58a0a9fa5fc4a7f3ac9e53
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-12 12:46:30 +0100

    Update header.php

--- buyer/header.php
+++ buyer/header.php
@@ -6,13 +6,18 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="alfacoins-site-verification" content="5ef8c2279aa605ef8c2279aa965ef8c2279aacb_ALFAcoins">
-    <meta name="revisit-after" content="2 days">
+    <meta name="revisit-after" content="content">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <script src="/cdn-cgi/apps/head/AGN3NmUziwZfMV-TNTe1kdyeR2Y.js"></script>
     <link rel="shortcut icon" href="../../favicon.ico" />
     <title>Report</title>
-    <link rel="stylesheet" href="layout/css/bootstrap.min.css">
+    
+    
+    <link rel="stylesheet" href="layout/css/all.Xbasetools.css">
+    <link rel="stylesheet" href="layout/css/all.Xbasetools.css">
+    <link rel="stylesheet" href="layout/css/all.Xbasetools.css">
+    <link rel="stylesheet" href="layout/css/all.Xbasetools.css">
+        
     <script src="layout/js/jquery-3.4.1.min.js"></script>
     <script src="layout/js/clipboard.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
