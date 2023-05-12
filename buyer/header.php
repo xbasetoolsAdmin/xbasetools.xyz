a5a33e6d40f087b1533b9e10048e2e4f9f7fae71
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-13 00:24:12 +0100

    Update header.php

--- buyer/header.php
+++ buyer/header.php
@@ -1,22 +1,24 @@
 <!DOCTYPE html>
-<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">  
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Main - home </title>
- <link rel="stylesheet" href="layout/css/bootstrap.min.css">
- <link rel="stylesheet" type="text/css" href="layout/css/main.css">
-<link rel="stylesheet" type="text/css" href="../buyer/assets/flags.css">
- <link rel="stylesheet" type="text/css" href="layout/css/tickets.css">
-  
-<script type="text/javascript" src="layout/dist/js/jquery-3.5.1.min.js"></script>
-<script type="text/javascript" src="layout/dist/js/jquery.dataTables.js"></script>
+                <title>Main - home </title>
+   <link rel="stylesheet"href="layout/css/bootstrap.min.css">
+   <link rel="stylesheet" type="text/css" href="layout/dist/css/main.css">
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js">
+  </script>
+	<style>
+		body {padding-top:80px }
+	</style>
+ <link rel="stylesheet" type="text/css" href="layout/flags.css">
+ <link rel="stylesheet" type="text/css" href="layout/dist/css/tickets.css">
+ <script type="text/javascript" src="layout/dist/js/jquery-3.5.1.min.js"></script>
+<script type="text/javascript" src="layout/dist/js/jquery.dataTables.min.js"></script>
 <script type="text/javascript" src="layout/dist/js/bootbox.min.js"></script>
-<script type="text/javascript" src="./assets/plugin.js"></script>
-  
-<font face="Arial"><link href="layout/css/style.css" rel="stylesheet">	
+<script type="text/javascript" src="assets/plugin.js"></script>
+ <font face="Arial"><link href="layout/css/style.css" rel="stylesheet">	
 <style>
 
 .alert.alert-shadowed {
