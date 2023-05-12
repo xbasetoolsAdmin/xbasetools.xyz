629004589dabc1fb2ae3cb431fdae1a514a80eb5
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-12 13:43:45 -0700

    Update header.php

--- buyer/header.php
+++ buyer/header.php
@@ -1,12 +1,5 @@
-<?php 
-date_default_timezone_set('UTC');
-include('../includes/config.php');
-
-if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
-    header("location: ../");
-    exit();
+<?php  date_default_timezone_set('UTC'); include('../includes/config.php'); if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) { header("location: ../");   exit();
 }
-$usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
-?>
+$usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']); ?>
 <!DOCTYPE html> head> <meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1"> <meta name="revisit-after" content="2 days"> <meta http-equiv="X-UA-Compatible" content="IE=edge"><link rel="shortcut icon" href="favicon.ico.png">
                      <title>Ticket</title>
