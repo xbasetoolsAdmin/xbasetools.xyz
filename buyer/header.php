c010c9845b5187d6080f19c85fd932b5d08ab0d1
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-12 08:07:19 -0700

    Update header.php

--- buyer/header.php
+++ buyer/header.php
@@ -1,77 +1,58 @@
+DOCTYPE html><html class="theme-light"><head>
+<meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="revisit-after" content="2 days">
+<meta http-equiv="X-UA-Compatible" content="IE=edge"><link rel="shortcut icon" href="favicon.ico.png">
+
+<title>Ticket</title>
+<link rel="stylesheet" href="layout/css/all.xbasetools.css"> 
+<link rel="stylesheet" href="layout/css/bs.xbasetools.min.css">
+<link rel="stylesheet" href="layout/css/bs.main.css">
+<link rel="stylesheet" href="layout/css/bs.xbasetools.css"> <link rel="stylesheet" href="layout/fonts/iconic/css/material-design-iconic-font.min.css"> <link href="../cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
+
+<link rel="stylesheet" type="text/css" href="layout/css/flags.css">
+<style>
+body{padding-top:80px}
+</style>
+
+<script src="layout/js/all.xbasetools.min.js">
+</script>
+<script src="layout/js/bootbox.min.js">
+</script>
+<script src="layout/js/jquery-3.5.1.min.js">
+</script>
+<script src="layout/js/clipboard.min.js">
+</script>
+<script src="../cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js">
+</script>
 
 
-<!DOCTYPE html>
-<html>
- 
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="revisit-after" content="content">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <script src="/cdn-cgi/apps/head/AGN3NmUziwZfMV-TNTe1kdyeR2Y.js"></script>
-    <link rel="shortcut icon" href="../../favicon.ico" />
-    <title>Report</title>
-    
-    
-    <link rel="stylesheet" href="layout/css/bs.xbasetools.css">
-    <link rel="stylesheet" href="layout/css/all.Xbasetools.css">
-    <link rel="stylesheet" href="layout/css/all.Xbasetools.css">
-    <link rel="stylesheet" href="layout/css/all.Xbasetools.css">
-        
-    <script src="layout/js/jquery-3.4.1.min.js"></script>
-    <script src="layout/js/clipboard.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <script src="layout/js/bootstrap.min.js"></script>
-    <script src="layout/js/bootbox.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="layout/css/flags.css" />
- 
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.6/css/responsive.dataTables.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.4/css/buttons.dataTables.min.css">
-    <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/responsive/2.2.6/js/dataTables.responsive.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.6.4/js/dataTables.buttons.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.6.4/js/buttons.colVis.min.js"></script>
-    <script src="js/jquery.dataTables.min.js"></script>
-    <link href="//cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
-    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js"></script>
- 
-    <script async src="//www.googletagmanager.com/gtag/js?id=UA-177092549-1"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
- 
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-        gtag('set', {
-            '$usrid': 'USER_ID'
-        }); // Set the user ID using signed-in user_id.
-        gtag('config', 'UA-177092549-1');
-    </script>
-    <link rel="stylesheet" href="layout/css/all.min.css" />
-    <link rel="stylesheet" href="layout/css/main.css?v=12.9" />
-    <link rel="stylesheet" href="layout/css/util.css" />
-    <style>
-        body {
-            padding-top: 80px
-        }
-    </style>
-    <link rel="stylesheet" href="layout/fonts/iconic/css/material-design-iconic-font.min.css">
-    <script src="layout/js/main.js"></script>
-    <script type="text/javascript">
-        // Notice how this gets configured before we load Font Awesome
-        window.FontAwesomeConfig = {
-            autoReplaceSvg: false
-        }
-    </script>
-    <style>
-        @import url(//fonts.googleapis.com/css?family=Roboto:400);
- 
-        .navbar-nav .dropdown-menu {
-            margin: 0 !important
-        }
-    </style>
+<link rel="stylesheet" href="../cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
+<link rel="stylesheet" href="../cdn.datatables.net/responsive/2.2.6/css/responsive.dataTables.min.css">
+<link rel="stylesheet" href="../cdn.datatables.net/buttons/1.6.4/css/buttons.dataTables.min.css">
+
+
+<script src="../cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+<script src="../cdn.datatables.net/responsive/2.2.6/js/dataTables.responsive.min.js"></script>
+<script src="../cdn.datatables.net/buttons/1.6.4/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.4/js/buttons.colVis.min.js"></script>
+<script src="js/jquery.dataTables.min.js">
+
+</script>
+<script type="text/javascript" src="../cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js">
+</script>
+by<style type="text/css">
+/* Chart.js */
+@-webkit-keyframes chartjs-render-animation{from{opacity:0.99}to{opacity:1}}@keyframes chartjs-render-animation{from{opacity:0.99}to{opacity:1}}.chartjs-render-monitor{-webkit-animation:chartjs-render-animation 0.001s;animation:chartjs-render-animation 0.001s;}</style><script src="layout/js/main.js">
+</script><script type="text/javascript">
+            // Notice how this gets configured before we load Font Awesome
+            window.FontAwesomeConfig = { autoReplaceSvg: false }
+        </script>
+<style>@import url(../fonts.googleapis.com/css%EF%B9%96family=Roboto%EF%B9%95400.css);
+            .navbar-nav .dropdown-menu
+            {
+            margin:0 !important
+            }
+        </style>
 	<script>
    $(document).ready(function () {
     $('#example').DataTable(); });
