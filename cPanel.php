277c33335389fe6d2af970b4a22280bfb682f96c
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-12 10:34:41 -0700

    Update cPanel.php

--- cPanel.php
+++ cPanel.php
@@ -32,6 +32,49 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 <title>JeruxShop</title>
 </head>
 <style>
+#table {
+  .sortable
+}
+table th:not(.sorttable_sorted):not(.sorttable_sorted_reverse):not(.sorttable_nosort):after { 
+    content: " \25BE" 
+}
+
+.label-as-badge {
+    border-radius: 0.5em;
+}
+
+body {
+    padding-top:50px;
+}
+table.floatThead-table {
+    border-top: none;
+    border-bottom: none;
+    background-color: #fff;
+}
+@media (min-width: 768px) {
+  .dropdown:hover .dropdown-menu {
+    display: block;
+  }
+}
+
+#mydiv {
+  height: 400px;
+  position: relative;
+}
+.ajax-loader {
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto; /* presto! */
+
+}
+
+   
+    
+
+</style>
 
 <script type="text/javascript">
              function ajaxinfo() {
