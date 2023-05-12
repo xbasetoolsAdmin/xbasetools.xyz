edaf1d92429cb5078eb29f7415dd2dee32da4858
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-12 08:20:41 -0700

    Update header.php

--- buyer/header.php
+++ buyer/header.php
@@ -7,46 +7,31 @@ DOCTYPE html><html class="theme-light"><head>
 <link rel="stylesheet" href="layout/css/all.xbasetools.css"> 
 <link rel="stylesheet" href="layout/css/bs.xbasetools.min.css">
 <link rel="stylesheet" href="layout/css/bs.main.css">
-<link rel="stylesheet" href="layout/css/bs.xbasetools.css"> <link rel="stylesheet" href="layout/fonts/iconic/css/material-design-iconic-font.min.css"> <link href="../cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
-
+<link rel="stylesheet" href="layout/css/bs.xbasetools.css"> 
+<link rel="stylesheet" href="layout/fonts/iconic/css/material-design-iconic-font.min.css"> <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="layout/css/flags.css">
 <style>
 body{padding-top:80px}
 </style>
-
 <script src="layout/js/all.xbasetools.min.js">
 </script>
-<script src="layout/js/bootbox.min.js">
-</script>
-<script src="layout/js/jquery-3.5.1.min.js">
-</script>
-<script src="layout/js/clipboard.min.js">
-</script>
-<script src="../cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js">
-</script>
+<script src="layout/js/bootbox.min.js"></script>
+<script src="layout/js/jquery-3.5.1.min.js"></script><script src="layout/js/clipboard.min.js">
+</script><script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.6/css/responsive.dataTables.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.4/css/buttons.dataTables.min.css">
 
-<link rel="stylesheet" href="../cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
-<link rel="stylesheet" href="../cdn.datatables.net/responsive/2.2.6/css/responsive.dataTables.min.css">
-<link rel="stylesheet" href="../cdn.datatables.net/buttons/1.6.4/css/buttons.dataTables.min.css">
-
-
-<script src="../cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-<script src="../cdn.datatables.net/responsive/2.2.6/js/dataTables.responsive.min.js"></script>
-<script src="../cdn.datatables.net/buttons/1.6.4/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script><script src="https://cdn.datatables.net/responsive/2.2.6/js/dataTables.responsive.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.4/js/dataTables.buttons.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/1.6.4/js/buttons.colVis.min.js"></script>
-<script src="js/jquery.dataTables.min.js">
-
-</script>
-<script type="text/javascript" src="../cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js">
-</script>
-by<style type="text/css">
-/* Chart.js */
-@-webkit-keyframes chartjs-render-animation{from{opacity:0.99}to{opacity:1}}@keyframes chartjs-render-animation{from{opacity:0.99}to{opacity:1}}.chartjs-render-monitor{-webkit-animation:chartjs-render-animation 0.001s;animation:chartjs-render-animation 0.001s;}</style><script src="layout/js/main.js">
+<script src="jquery.dataTables.min.js"></script><script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js">
+</script><style type="text/css">/* Chart.js */@-webkit-keyframes chartjs-render-animation{from{opacity:0.99}to{opacity:1}}@keyframes chartjs-render-animation{from{opacity:0.99}to{opacity:1}}.chartjs-render-monitor{-webkit-animation:chartjs-render-animation 0.001s;animation:chartjs-render-animation 0.001s;}</style><script src="layout/js/main.js">
 </script><script type="text/javascript">
             // Notice how this gets configured before we load Font Awesome
             window.FontAwesomeConfig = { autoReplaceSvg: false }
-        </script>
+</script>
 <style>@import url(../fonts.googleapis.com/css%EF%B9%96family=Roboto%EF%B9%95400.css);
             .navbar-nav .dropdown-menu
             {
