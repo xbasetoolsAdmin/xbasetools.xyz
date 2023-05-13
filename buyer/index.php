7600f284f83a4f9a611769bbc8dd5cf8ce801c1f
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-13 18:12:14 +0100

    Update index.php

--- buyer/index.php
+++ buyer/index.php
@@ -1,5 +1,7 @@
 <?php require('header.php');?>
-
+<script>
+  $(function(){$("*[divPage-html]").each(function(){var t=$(this).attr("divPage-html"),u=this.id;jQuery.ajax({url:t,success:function(t){$("#"+u).html(t)},error:function(n,c,i){var s=n.status+": "+n.statusText;$("#"+u).html(t+"-"+s)}})})});
+ </script>
 
 
 
