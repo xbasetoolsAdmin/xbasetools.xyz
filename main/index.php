361d90527b922fe90ec0c812c427d99f4d45932c
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-15 00:31:40 +0100

    Update index.php

--- main/index.php
+++ main/index.php
@@ -8,6 +8,276 @@ include "header.php";
 <?php 
 include "navbar_header.php";
 ?>
+<style>.scroll-area-lg {
+    height: 400px;
+    overflow-x: hidden;
+}
+.scrollbar-sidebar, .scrollbar-container {
+    position: relative;
+    height: 100%;
+}
+.ps {
+    overflow: hidden !important;
+    overflow-anchor: none;
+    touch-action: auto;
+}
+.heading{
+     font-size: 16px;
+     font-weight: bold;
+    }
+ .fa-plus-circle {
+      color: green;
+    }
+#tour {
+    font-family: 'Raleway', sans-serif;
+}
+
+</style>
+
+<link rel="stylesheet" href="layout/css/flexslider5.css" type="text/css">
+<script src="layout/js/jquery.flexslider-min.js"></script>
+<style>#tour {
+    font-family: 'Raleway', sans-serif;
+}
+
+#tour .flexslider {
+    margin: 0 0 60px;
+    background: #fff;
+    border: 0px solid #fff;
+    position: relative;
+    zoom: 0;
+    -webkit-border-radius: 0px;
+    -moz-border-radius: 0px;
+    border-radius: 0px;
+    -webkit-box-shadow: '' 0 0px 0px rgba(0, 0, 0, 0.2);
+    -moz-box-shadow: '' 0 0px 0px rgba(0, 0, 0, 0.2);
+    -o-box-shadow: '' 0 0px 0px rgba(0, 0, 0, 0.2);
+    box-shadow: '' 0 0px 0px rgba(0, 0, 0, 0.2);
+}
+
+
+
+#tour .flex-control-paging li a {
+    width: 11px;
+    height: 11px;
+    display: block;
+    background: #fff!important;
+    border: 1px solid #3c8dbc;
+    background: rgba(0, 0, 0, 0.5);
+    cursor: pointer;
+    text-indent: -9999px;
+    -webkit-box-shadow: inset 0 0 0px rgba(0, 0, 0, 0);
+    -moz-box-shadow: inset 0 0 0px rgba(0, 0, 0, 0);
+    -o-box-shadow: inset 0 0 0px rgba(0, 0, 0, 0);
+    box-shadow: inset 0 0 0px rgba(0, 0, 0, 0);
+    -webkit-border-radius: 20px;
+    -moz-border-radius: 20px;
+    border-radius: 20px;
+}
+
+#tour .flex-control-paging li a.flex-active {
+    background: #3c8dbc!important;
+}
+
+#tour .modal-body {
+    padding: 0px;
+}
+
+#tour .modal-footer {
+    background: #fafafa;
+    border:0px;
+}
+
+#tour .flex-caption {
+    padding: 20px 100px 20px 100px;
+    text-align: center;
+}
+
+#tour .flex-caption .heading{
+    font-weight: 600;
+    font-size: 20px;
+    color: #505050;
+    padding-bottom: 15px;
+}
+
+#tour .flex-caption .caption-content{
+    font-size: 15px;
+    color: #505050;
+}
+
+</style>
+
+<div class="modal fade top" id="mod" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="true">
+<div class="modal-dialog modal-frame modal-top modal-notify modal-danger" role="document">
+
+<div class="modal-content">
+
+<div class="modal-body">
+<div id="bodyTicket" class="row d-flex justify-content-center align-items-center">
+<span class="pt-3 mx-4" style="font-size: 14 px"><b>You have new replay ticket</b></span><b>
+<a type="button" href="tickets.html" class="btn btn-success waves-effect waves-light">Show tickets
+</a>
+</b></div><b>
+</b></div><b>
+</b></div><b>
+
+</b></div><b>
+</b></div><b>
+
+
+<div class="modal fade top" id="modalrep" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="true">
+<div class="modal-dialog modal-frame modal-top modal-notify modal-danger" role="document">
+
+<div class="modal-content">
+
+<div class="modal-body">
+<div id="bodyreport" class="row d-flex justify-content-center align-items-center">
+<span class="pt-3 mx-4" style="font-size: 14 px"><b>You have new replay report</b></span><b>
+<a type="button" href="reports.html" class="btn btn-danger waves-effect waves-light">Show reports
+</a>
+</b></div><b>
+</b></div><b>
+</b></div><b>
+
+</b></div><b>
+</b></div><b>
+
+
+<div class="modal fade top" id="modalreps" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="true">
+<div class="modal-dialog modal-frame modal-top modal-notify modal-danger" role="document">
+
+<div class="modal-content">
+
+<div class="modal-body">
+<div id="bodyreport" class="row d-flex justify-content-center align-items-center">
+<span class="pt-3 mx-4" style="font-size: 14 px"><b>You have new report</b></span><b>
+<a type="button" href="login.html" class="btn btn-danger waves-effect waves-light">Show reports
+</a>
+</b></div><b>
+</b></div><b>
+</b></div><b>
+
+</b></div><b>
+</b></div><b>
+
+
+
+<div class="modal fade top" id="modalrepss" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="true">
+<div class="modal-dialog modal-frame modal-top modal-notify modal-danger" role="document">
+
+<div class="modal-content">
+
+<div class="modal-body">
+<div id="bodyreport" class="row d-flex justify-content-center align-items-center">
+<span class="pt-3 mx-4" style="font-size: 14 px"><b>You have new replay report</b></span><b>
+<a type="button" href="login.html" class="btn btn-danger waves-effect waves-light">Show reports
+</a>
+</b></div><b>
+</b></div><b>
+</b></div><b>
+
+</b></div><b>
+</b></div><b>
+
+<div class="d-flex flex-row-reverse mt-0" style="z-index: 999999;position: absolute; right: 0; ">
+<div class="p-2"><label id="switch" class="switch">
+<input type="checkbox" onchange="toggleTheme()" id="slider">
+<span class="slider round"></span>
+</label></div>
+</div>
+<script defer="" src="layout/js/jquery.flexslider.js"></script>
+<div class="header">
+<div class="container-fluid">
+<div class="row">
+<div class="col-lg-3 col-md-6 col-sm-12" style="text-align:center;"><a href="shell.html"><img style="border: 3px solid;" src="layout/images/shells.gif"></a></div>
+<div class="col-lg-3 col-md-6 col-sm-12" style="text-align:center;"><a href="smtp.html"><img style="border: 3px solid;" src="layout/images/smtps.gif"></a></div>
+<div class="col-lg-3 col-md-6 col-sm-12" style="text-align:center;"><a href="accounts-7.html"><img style="border: 3px solid;" src="layout/images/accs.gif"></a></div>
+</div><br>
+<div class="header-body">
+
+<div class="row">
+<div class="col-md-3 col-sm-6">
+<div class="card card-stats">
+<div class="card-body">
+<div class="row">
+<div class="col-md-10">
+<h5 class="card-title text-uppercase text-muted mb-0">Balance</h5>
+<span class="h2 font-weight-bold mb-0">
+0 </span>
+</div>
+<div class="col-md-2">
+<div class="icon icon-shape bg-danger text-white rounded-circle shadow">
+<i class="fa fa-money-bill-alt "></i>
+</div>
+</div>
+</div>
+ <p class="mt-3 mb-0 text-muted text-sm">
+<span class="text-nowrap"><a href="addBalance.html"> [Add Funds] </a></span>
+</p>
+</div>
+</div>
+</div>
+<div class="col-md-3 col-sm-6">
+<div class="card card-stats">
+<div class="card-body">
+<div class="row">
+<div class="col-md-10">
+<h5 class="card-title text-uppercase text-muted mb-0">Orders</h5>
+<span class="h2 font-weight-bold mb-0">0 </span></div>
+<div class="col-md-2">
+<div class="icon icon-shape bg-info text-white rounded-circle shadow">
+<i class="fa fa-shopping-cart"></i>
+</div>
+</div>
+</div>
+<p class="mt-3 mb-0 text-muted text-sm">
+<span class="text-nowrap"><a href="orders.html"> [ Show ] </a></span>
+</p>
+</div>
+</div>
+</div>
+<div class="col-md-3 col-sm-6">
+<div class="card card-stats">
+<div class="card-body">
+<div class="row">
+<div class="col-md-10">
+<h5 class="card-title text-uppercase text-muted mb-0">Tickets</h5>
+<span class="h2 font-weight-bold mb-0">0 </span></div>
+<div class="col-md-2">
+<div class="icon icon-shape bg-warning text-white rounded-circle shadow">
+<i class="fa fa-cog"></i>
+</div>
+</div>
+</div>
+<p class="mt-3 mb-0 text-muted text-sm">
+<span class="text-nowrap"><a href="tickets.html"> [ Show ]</a></span>
+</p>
+</div>
+</div>
+</div>
+<div class="col-md-3 col-sm-6">
+<div class="card card-stats" style="background-color: #fb3!important;">
+<div class="card-body">
+<div class="row">
+<div class="col-md-10">
+<h5 class="card-title text-uppercase text-muted mb-0">Reports</h5>
+<span class="h2 font-weight-bold mb-0">
+0 <span>
+</span></span></div>
+<div class="col-md-2">
+<div class="icon icon-shape bg-danger text-white rounded-circle shadow">
+<i class="fas fa-receipt"></i>
+</div>
+</div>
+</div>
+<p class="mt-3 mb-0 text-muted text-sm">
+<span class="text-nowrap"><a href="reports.html"> [ Show ]</a></span>
+</p>
+</div>
+</div>
+</div>
+</div>
+</div>
 
 
 
