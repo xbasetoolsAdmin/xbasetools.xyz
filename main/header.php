b0ca4d095f73746668e0af9b0b0613701012b747
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-15 03:08:55 +0100

    Update header.php

--- main/header.php
+++ main/header.php
@@ -1,46 +1,55 @@
 <?php  ob_start(); session_start(); date_default_timezone_set('UTC'); include "../includes/config.php"; if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {    header("location: ../");     exit(); } $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']); ?>
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width" />
-    <title> Main </title>
-    <!------------Bootstrap CSS library------------>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.3/css/bootstrap.min.css" />
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name=" " content="  ">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<link rel="shortcut icon" href="favicon.ico.png">
+<title>xBaseTools</title>
+<!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.3/css/bootstrap.min.css" /> <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.3/js/bootstrap.bundle.min.js">
+</script>--->
+<link rel="stylesheet" href="layout/css/all.min.css" />
+<link rel="stylesheet" href="layout/css/main.css" />
+<link rel="stylesheet" href="layout/css/util.css" />
+<link rel="stylesheet" href="layout/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="layout/css/flags.css" />
+<style>body{padding-top:80px}</style>
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.6/css/responsive.dataTables.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.4/css/buttons.dataTables.min.css">
+<link rel="stylesheet" href="layout/fonts/iconic/css/material-design-iconic-font.min.css">
+<script async src="//www.googletagmanager.com/gtag/js?id=UA-177092549-1">
     
+</script>
+<script>window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('set', {'$usrid': 'USER_ID'}); // Set the user ID using signed-in user_id.
+gtag('config', 'UA-177092549-1');
+</script>
+<script src="layout/js/clipboard.min.js"></script>
+<script src="layout/js/bootstrap.min.js"></script>
+<script src="layout/js/bootbox.min.js">
     
-    <!----------MDBootstrap assets library--------->
-
-<!---------------jquery JS library------------>
-  
-    <script src="https://code.jquery-3.5.1.min.js"></script>
-  
-<!---------cdn dataTable css library--------->
-
-<!---------cdn dataTable js library--------->
-
-<!---------cdn  js component library--------->
-
-<!---------xbasetools text/js library--------->
-
-
-    <script type="text/javascript">
-        // Notice how this gets configured before we load Font Awesome
-        window.FontAwesomeConfig = {
-            autoReplaceSvg: false
-        }
-    </script>
+</script>
+<script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.2.6/js/dataTables.responsive.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.4/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.4/js/buttons.colVis.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js">
     
-    <!---------cdn dataTable css library--------->
-    
-    <style>
-        @import url(//fonts.googleapis.com/css?family=Roboto:400);
- 
-        .navbar-nav .dropdown-menu {
-            margin: 0 !important
-        }
+</script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="js/jquery.dataTables.min.js"></script>
+<script src="layout/js/main.js"></script>
+<script type="text/javascript">
+// Notice how this gets configured before we load Font Awesome
+window.FontAwesomeConfig = { autoReplaceSvg: false }
+</script>
+<style>
+@import url(//fonts.googleapis.com/css?family=Roboto:400);
+.navbar-nav .dropdown-menu
+{
+margin:0 !important
+}
 </style>
-
-<!---------cdn dataTable css library--------->
-
 </head>
