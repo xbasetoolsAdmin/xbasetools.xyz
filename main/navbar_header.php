b292c14c15249d63acd77cf3e5eb61e19dfc6d67
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-15 11:13:51 +0100

    Update navbar_header.php

--- main/navbar_header.php
+++ main/navbar_header.php
@@ -1,4 +1,9 @@
-<?php include "../includes/config.php"; ?>
+<?php 
+
+include "../includes/config.php";
+
+ ?>
+
 <?php  ob_start(); session_start(); date_default_timezone_set('UTC'); include "../includes/config.php"; if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {    header("location: ../");     exit(); } $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']); ?>
 	
 		<style>
@@ -350,7 +355,5 @@ a.closearb {
 </label>
 	</div>
 </div>
-	
 	<script src="layout/js/main.js"></script>
- 
-<div class="container" id="">
+<div class="container" id="mainDiv">
