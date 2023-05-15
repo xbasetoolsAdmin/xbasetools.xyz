623481fefe4b7a54899ef0e47bf703979c51252c
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-15 01:54:41 +0100

    Update header.php

--- main/header.php
+++ main/header.php
@@ -1,39 +1,21 @@
 <?php  ob_start(); session_start(); date_default_timezone_set('UTC'); include "../includes/config.php"; if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {    header("location: ../");     exit(); } $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']); ?>
 <!DOCTYPE html><html><head><meta charset="UTF-8"> <meta name="viewport" content="width=device-width" />
                
-                                     <title> Main </title>
+                                               <title> Main </title>
   
 <!------------Bootstrap CSS library------------>
 
-
-
-
-
 <!----------MDBootstrap assets library--------->
 
-
-
-
-
-
 <!---------------jquery JS library------------>
-
-
+  
     <script src="https://code.jquery-3.5.1.min.js"></script>
-
-
+  
 <!---------cdn dataTable css library--------->
-
-
 
 <!---------cdn dataTable js library--------->
 
- 
 <!---------cdn  js component library--------->
-
-l
-
-
 
 <!---------xbasetools text/js library--------->
 
