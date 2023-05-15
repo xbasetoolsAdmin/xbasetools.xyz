8d340898824d1b9412e9dbc798de7d0f1b9b2463
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-15 01:32:00 +0100

    Update and rename ajax.json to rdp.php

---
+++ main/rdp.php
@@ -0,0 +1,23 @@
+<?php include "header.php";?>
+<div class="divPage.html" id="accounts"></div>
+
+             
+
+
+
+       <h2>I m Main</h2>
+                
+
+
+
+
+<div class="footer.html" id="footer_file"></div>
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="divPage.min.js"></script>
+
+
+<?php include "footer.php";?>
